7b0ea1b33e573fb940bad5403d4624514fcd81c7
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2023-12-03 12:33:00 +0100

    esercizi basici

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,73 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      //Scrivi un programma Java che dichiara una variabile intera chiamata
+        // "numero" e le assegna il valore 10. Stampa il valore della variabile.
+
+        int numero=10;
+        System.out.println(numero);
+        //Scrivi un programma Java che dichiara una variabile di tipo float chiamata "altezza"
+        // e le assegna il valore 1.75. Stampa il valore della variabile.
+
+        float altezza=1.75f;
+        System.out.println(altezza);
+
+        //Scrivi un programma Java che dichiara una variabile di tipo boolean chiamata
+        // "isPieno" e le assegna il valore true. Stampa il valore della variabile.
+
+        boolean isPieno=true;
+        System.out.println(isPieno);
+
+
+        //Scrivi un programma Java che dichiara una variabile di tipo char chiamata "lettera"
+        // e le assegna il valore 'A'. Stampa il valore della variabile.
+
+
+        char lettera='A';
+        System.out.println(lettera);
+
+
+        //Scrivi un programma Java che dichiara una variabile di tipo String chiamata
+        // "nome" e le assegna il valore "Mario Rossi". Stampa il valore della variabile.
+
+        String nome="Mario Rossi";
+        System.out.println(nome);
+
+
+        //Scrivi un programma Java che dichiara una variabile di tipo double chiamataù
+        // "peso" e le assegna il valore 68.5. Stampa il valore della variabile.
+
+
+        double peso=68.5;
+        System.out.println(peso);
+
+
+
+        //Scrivi un programma Java che dichiara due variabili intere chiamate "numero1" e "numero2" e
+        // le assegna i valori 5 e 10 rispettivamente. Calcola la somma delle due variabili e
+        // stampa il risultato.
+
+
+
+        int numero1=5;
+        int numero2=10;
+
+        System.out.println(numero1 + numero2);
+
+
+        //Scrivi un programma Java che dichiara una variabile di tipo long chiamata
+        // "popolazioneMondiale" e le assegna il valore 7800000000. Stampa il valore della variabile.
+
+
+        long popolazioneMondiale=7800000000L;
+
+        System.out.println(popolazioneMondiale);
+
+        //Scrivi un programma Java che dichiara una variabile di tipo byte chiamata
+        // "eta" e le assegna il valore 25. Stampa il valore della variabile.
+
+
+        byte eta=25;
+        System.out.println(eta);
+
     }
 }
