fda01a00dc063707b31e25170443f2efcd2a3221
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2023-12-03 13:09:57 +0100

    cast

--- src/Main.java
+++ src/Main.java
@@ -69,5 +69,107 @@ public class Main {
         byte eta=25;
         System.out.println(eta);
 
+        //Scrivi un programma Java che dichiara una variabile di tipo intero chiamata "numero" e una variabile di tipo
+        // double
+        // chiamata "numeroDecimale". Esegui un cast della variabile "numero" in double e assegna il
+        // valore risultante alla variabile "numeroDecimale". Stampa il valore della variabile "numeroDecimale".
+
+
+         int numer=10;
+         double numeroDecimale=(double) numer; //ecco come si fa un cast;
+         System.out.println(numeroDecimale);
+
+         //Scrivi un programma Java che dichiara una variabile di tipo double chiamata "numeroDecimale"
+        // e una variabile di tipo int chiamata
+        // "numeroIntero". Esegui un cast della variabile "numeroDecimale" in int e assegna il
+        // valore risultante alla variabile "numeroIntero". Stampa il valore della variabile "numeroIntero"
+
+
+        double numeroDecimal=12.5;
+        int numeroIntero=(int) numeroDecimal;
+
+        System.out.println(numeroIntero);
+
+        //Scrivi un programma Java che dichiara una variabile di tipo int chiamata "numeroIntero"
+        // e una variabile di tipo String chiamata "numeroStringa". Esegui un cast della variabile "numeroIntero"
+        // in String e assegna il valore
+        // risultante alla variabile "numeroStringa". Stampa il valore della variabile "numeroStringa".
+
+        int numeroInter=26;
+        String numeroStringa=String.valueOf(numeroInter);//per convertire un qualcosa a stringa si usa String.value0f;
+
+        System.out.println(numeroStringa);
+
+        //Scrivi un programma Java che dichiara una variabile di tipo String chiamata "numeroStringa"
+        // e una variabile di tipo int chiamata "numeroIntero". Esegui un cast della variabile "numeroStringa"
+        // in int e assegna
+        // il valore risultante alla variabile "numeroIntero". Stampa il valore della variabile "numeroIntero".
+
+        String numeroString="28308302";
+        int numeroInte=Integer.parseInt(numeroString);//sistema per convertire da stringa a numero;
+
+        System.out.println(numeroInte);
+
+        //Scrivi un programma Java che dichiara una variabile di tipo double chiamata "numeroDecimale" e una variabile
+        // di tipo int chiamata "numeroIntero". Esegui un cast della variabile "numeroDecimale" in int e assegna
+        // il valore risultante alla variabile "numeroIntero". Stampa il valore della variabile "numeroIntero"
+
+        double numeroDecima=10.99;
+        int numeroInt=(int)numeroDecima;
+
+        System.out.println(numeroInt);
+
+
+        //Scrivi un programma Java che dichiara una variabile di tipo long chiamata "numeroLungo"
+        // e una variabile di tipo int chiamata "numeroIntero". Esegui un cast della
+        // variabile "numeroLungo" in int e assegna il valore risultante
+        // alla variabile "numeroIntero". Stampa il valore della variabile "numeroIntero"
+
+
+        long numeroLungo=82468726L;
+
+        int numeroIn=(int) numeroLungo;
+
+        System.out.println(numeroIn);
+
+
+        //Scrivi un programma Java che dichiara una variabile di tipo float chiamata "numeroVirgola"
+        // e una variabile di tipo double chiamata "numeroDecimale".
+        // Esegui un cast della variabile "numeroVirgola" in double e assegna il valore risultante
+        // alla variabile "numeroDecimale". Stampa il valore della variabile "numeroDecimale".
+
+
+        float numeroVirgola=63468f;
+        double numeroDe=(double) numeroVirgola;
+        System.out.println(numeroDe);
+
+        //Scrivi un programma Java che dichiara una variabile di tipo byte chiamata "numeroPiccolo"
+        // e una variabile di
+        // tipo int chiamata "numeroGrande". Esegui un cast della variabile "numeroPiccolo" in int e assegna
+        // il valore
+        // risultante alla variabile "numeroGrande". Stampa il valore della variabile "numeroGrande".
+
+
+        byte numeroPiccolo=27;//se non ricordo male va -128/127;
+
+        int numeroGrande=(int) numeroPiccolo;
+
+        System.out.println(numeroGrande);
+
+        //Scrivi un programma Java che dichiara una variabile di tipo double chiamata "numeroDecimale"
+        // e una variabile
+        // di tipo String chiamata "numeroStringa". Esegui un cast della variabile "numeroStringa"
+        // in double e assegna
+        // il valore risultante alla variabile "numeroDecimale". Stampa il valore della
+        // variabile "numeroDecimale".
+
+        double numeroD=12.2;
+        String numeroStr=String.valueOf(numeroD);
+
+        System.out.println(numeroStr);
+
+        
+
+
     }
 }
